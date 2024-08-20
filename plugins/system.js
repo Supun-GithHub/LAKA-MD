d70b3967ee007d37ffc0cc16bff4705bd2dec8c5
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-20 18:58:25 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,18 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "Check up time , ram usage and more",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
