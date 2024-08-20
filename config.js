cacc5bfd2bf1ebc4b4d722a1f265c430210fed7b
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-20 15:43:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/20814386ac8526c7d91aa.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "
-```💗 LAKA-MD WHATSAPP BOT💗```
-
-LAKA-MD is designed to facilitate other functions within WhatsApp 📱🪄
-
-I will always help you Love you forever 🍂✨
-
-💗 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙻𝙰𝙺𝙰-𝙾𝙵𝙲 💗
-",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i'm Laka-md Bot Alive Now!"
 };
