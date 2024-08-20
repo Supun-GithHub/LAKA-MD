a34f5c2c6759bf552f2378e9b3eb614a721db975
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-20 17:07:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/20814386ac8526c7d91aa.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i'm Laka-md Bot Alive Now!"
+ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i'm Laka-md Bot Alive Now!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
