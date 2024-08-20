288f24848b0be4180095c79af21196296aa12c2e
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-20 19:25:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/20814386ac8526c7d91aa.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i'm Laka-md Bot Alive Now!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
 };
